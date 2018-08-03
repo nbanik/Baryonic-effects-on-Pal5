7653f1bc74a98e7844d26627ddc0aeb31f825fce
nbanik <nilbanik1@gmail.com>
2018-08-03 15:33:24 +0200

    more tests

--- make_stream_Plummer_different_pot.py
+++ make_stream_Plummer_different_pot.py
@@ -15,6 +15,7 @@ from optparse import OptionParser
 from galpy.util import bovy_conversion
 import pal5_util
 import pal5_util_MWfit
+import GMC_util
 import MWPotential2014Likelihood
 _REFR0, _REFV0= MWPotential2014Likelihood._REFR0, MWPotential2014Likelihood._REFV0
 ro, vo= _REFR0, _REFV0
@@ -26,13 +27,11 @@ def get_options():
     parser = OptionParser(usage=usage)
     
                           
-    parser.add_option("--ind",dest='ind',default=None,
+    parser.add_option("--chain_ind",dest='chain_ind',default=None,
                       type='int',
                       help="index of line from parama_file to be used to compute streampeppered object")
-                      
-    parser.add_option("--param_file",dest='param_file',default='rperi_grid_select.dat',
-                      help="name of selected parameters")
-                      
+    
+    #set timpact chunks                        
     parser.add_option("-t","--timpacts",dest='timpacts',default=None,
                       help="Impact times in Gyr to consider; should be a comma separated list")
     
@@ -73,34 +72,11 @@ for ii in range(nchunk):
     
 timpactn=tim_array[options.tind]
 
-##########setup Pal5 orbit and potential
-paramf=numpy.genfromtxt(options.param_file,delimiter=',')  
-
-pind=paramf[options.ind][0]
-
-peri=round(paramf[options.ind][1],2)
-print (peri)
-
-
-flat_c=paramf[options.ind][2]
-vc=paramf[options.ind][3]
-orb=list(paramf[options.ind][4:]) 
-tvo= vc*_REFV0
-
-#indices greater than 14: subtract 1
-if pind > 14 :
-    pind -=1
-    
-pind=int(pind)   
-potparams_file=numpy.loadtxt('pot_params.dat',delimiter=',')
-potparams=list(potparams_file[pind])
-   
-pot= MWPotential2014Likelihood.setup_potential(potparams,flat_c,False,False,
-                                                       pal5_util_MWfit._REFR0,tvo)
-
             
 
 #sdf_smooth= pal5_util.setup_pal5model()
-pepperfilename= 'pkl_files/pal5pepper_Plummer_{}sampling_pot{}_peri{}_{}.pkl'.format(len(timpacts),pind,peri,options.tind)
-sdf_pepper= pal5_util.setup_pal5model(timpact=timpactn,pot=pot,hernquist=False)
+pepperfilename= 'pkl_files/pal5pepper_Plummer_{}sampling_chainind{}_{}.pkl'.format(len(timpacts),options.chain_ind,options.tind)
+
+sdf_pepper=GMC_util.make_nondefault_pal5stream(options.chain_ind,timpact=timpactn)
+
 save_pickles(pepperfilename,sdf_pepper)
